0c1e795061077a8bda4d72a7c0c14019181f83c5
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2024-08-09 00:11:19 +0800

    feat: add ContentProcessor class

---
+++ processor.py
@@ -0,0 +1,5 @@
+class ContentProcessor:
+    def __init__(self, video_path: str, audio_path: str, combined_path: str):
+        self.video_path = video_path
+        self.audio_path = audio_path
+        self.combined_path = combined_path
